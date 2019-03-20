bb186fe27b20ba53cef6305c1d94c0d2a855fde3
jasonvank <fwenkai.jason@gmail.com>
2019-03-20 17:51:34 +0800

    update database env

--- App/routes/select.js
+++ App/routes/select.js
@@ -2,16 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const { Pool } = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: '********',
-  port: 5432,
-})
+
+const pool = new Pool({connectionString: process.env.DATABASE_URL});
 
 /* SQL Query */
 var sql_query = 'SELECT * FROM student_info';
+
 
 router.get('/', function(req, res, next) {
 	pool.query(sql_query, (err, data) => {

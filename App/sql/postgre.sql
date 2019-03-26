104c653e8d4a1503dbf08b7fbe99e5404cd8de1a
hotarumin <hotarumin98@gmail.com>
2019-03-26 15:22:28 +0800

    create tables in sql

--- App/sql/postgre.sql
+++ App/sql/postgre.sql
@@ -34,8 +34,140 @@ CREATE TABLE customers (
 
 CREATE TABLE assigns (
     brid    char(36),
-    resid   char(36),
-    primary key (brid, resid)
+    rmid   char(36),
+    primary key (brid, rmid),
+    foreign key (brid) references branch_managers.uid,
+    foreign key (rmid) references restaurant_managers.uid,
+);
+
+CREATE TABLE restaurants (
+    rid     uuid DEFAULT uuid_generate_v4 (),
+    name    varchar(50) NOT NULL, 
+    primary key (rid)
+);
+
+CREATE TABLE branches (
+    bid     uuid DEFAULT uuid_generate_v4 (),
+    name    varchar(50),
+    location varchar(50) NOT NULL,
+    opentime TIME NOT NULL,
+    closetime TIME NOT NULL,
+    aveRating NUMERIC(2,1) NOT NULL DEFAULT 5.0,
+    contacts  NUMERIC(10,0) NOT NULL,
+    CHECK (aveRating >= 0.0 and <= 5.0),
+    primary key (bid),
+    foreign key (name) references restaurants.name on delete cascade on update cascade
+);
+
+CREATE TABLE manages (
+    uid     char(36) NOT NULL,
+    bid     char(36) NOT NULL,
+    primary key (uid, bid),
+    foreign key (uid) references branch_managers.uid,
+    foreign key (bid) references branches.bid
+);
+
+CREATE TABLE opens (
+    rid   char(36) NOT NULL,
+    bid     char(36) NOT NULL,
+    primary key (rid, bid),
+    foreign key (rid) references restaurants.rid,
+    foreign key (bid) references branches.bid
+);
+
+CREATE TABLE registers (
+    uid     char(36) NOT NULL,
+    rid   char(36) NOT NULL,
+    primary key (uid, rid),
+    foreign key (uid) references restaurant_managers.uid,
+    foreign key (rid) references restaurants.rid
+);
+
+CREATE TABLE categories (
+    cid    uuid DEFAULT uuid_generate_v4 (),
+    name   varchar(50) NOT NULL,
+    primary key (cid)
+);
+
+CREATE TABLE belongs (
+    cid     char(36) NOT NULL,
+    rid     char(36) NOT NULL,
+    primary key (cid, rid),
+    foreign key (cid) references categories.cid,
+    foreign key (rid) references restaurants.rid
+);
+
+CREATE TABLE menus (
+    name  varchar(50),
+    mid   uuid DEFAULT uuid_generate_v4 (),
+    bid   varchar(50) NOT NULL,
+    primary key (mid),
+    foreign key (bid) references branches.bid on delete cascade
+);
+
+CREATE TABLE provides (
+    bid     char(36) NOT NULL,
+    mid     char(36) NOT NULL,
+    primary key (bid, mid),
+    foreign key (bid) references branches.bid,
+    foreign key (mid) references menus.mid
+);
+
+CREATE TABLE items (
+    iid     uuid DEFAULT uuid_generate_v4 (),
+    name    varchar(50) NOT NULL,
+    price   NUMERIC(3,2) NOT NULL,
+    description text,
+    mid     char(36), NOT NULL,
+    primary key (iid),
+    foreign key (mid) references menus.mid on delete cascade
+);
+
+CREATE TABLE contains (
+    iid     char(36) NOT NULL,
+    mid     char(36) NOT NULL,
+    primary key (iid, mid),
+    foreign key (iid) references items.iid,
+    foreign key (mid) references menus.mid
+);
+
+CREATE TABLE reservations (
+    resid   uuid DEFAULT uuid_generate_v4 (),
+    restime timestamp NOT NULL,
+    numpeople integer NOT NULL,
+    --bid     char(36) NOT NULL,
+    primary key (resid)
+    --, foreign key (bid) references branches.bid
+);
+
+CREATE TABLE processes (
+    resid     char(36) NOT NULL,
+    bid       char(36) NOT NULL,
+    primary key (resid, bid),
+    foreign key (resid) references reservations.resid,
+    foreign key (bid) references branches.bid
+);
+
+CREATE TABLE books (
+    resid     char(36) NOT NULL,
+    uid       char(36) NOT NULL,
+    primary key (resid, uid),
+    foreign key (resid) references reservations.resid,
+    foreign key (uid) references customers.uid
+);
+
+CREATE TABLE rewards (
+    rewid   uuid DEFAULT uuid_generate_v4 (),
+    value   NUMERIC(3,2) NOT NULL,
+    primary key (rewid)
+);
+
+CREATE TABLE earns (
+    rewid     char(36) NOT NULL,
+    uid       char(36) NOT NULL,
+    primary key (rewid, uid),
+    foreign key (rewid) references rewards.rewid,
+    foreign key (uid) references customers.uid
 );
 
 INSERT INTO users (username, password_hash, first_name, last_name)

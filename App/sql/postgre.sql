5cb5c136f23e9c6b9c3fbb5e8068a7d9d23d93a3
jasonvank <fwenkai.jason@gmail.com>
2019-03-29 13:51:48 +0800

    update sql tables

--- App/sql/postgre.sql
+++ App/sql/postgre.sql
@@ -1,25 +1,22 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
-  user_uid   uuid DEFAULT uuid_generate_v4 (),
-	username   varchar(255) NOT NULL,
+    user_uid        uuid DEFAULT uuid_generate_v4 (),
+	username        varchar(255) NOT NULL,
 	password_hash   varchar(64) NOT NULL,
-  first_name varchar(64) NOT NULL,
-	last_name  varchar(64) NOT NULL,
-  primary key (user_uid),
-  unique(username)
+    first_name      varchar(64) NOT NULL,
+	last_name       varchar(64) NOT NULL,
+    primary key (user_uid),
+    unique(username)
 );
 
-INSERT INTO users (username, password_hash, first_name, last_name)
-VALUES ('admini', '$2b$10$13BWk/6YJ4JYlxPvkNTnqeT6J8zsPTe592QIen.Le7apc921uebUW', 'Adi', 'Prabawa');
+CREATE TABLE branch_managers (
+	uid     uuid primary key references users on delete cascade
+);
 
 CREATE TABLE restaurant_managers (
 	uid     uuid primary key references users on delete cascade
 );
-
-INSERT INTO restaurant_managers (uid)
-SELECT user_uid
-FROM users;
 
 CREATE TABLE restaurants (
     rid     uuid DEFAULT uuid_generate_v4 (),
@@ -30,25 +27,23 @@ CREATE TABLE restaurants (
 );
 
 CREATE TABLE branches (
-    bid     uuid UNIQUE DEFAULT uuid_generate_v4 (),
-    name    varchar(50),
-    rid     uuid UNIQUE NOT NULL references restaurants (rid),
-    location varchar(50) NOT NULL,
-    opentime TIME NOT NULL,
-    closetime TIME NOT NULL,
-    aveRating NUMERIC(2,1) NOT NULL DEFAULT 5.0,
-    contacts  NUMERIC(10,0) NOT NULL,
+    bid          uuid UNIQUE DEFAULT uuid_generate_v4 (),
+    name         varchar(50),
+    rid          uuid UNIQUE NOT NULL references restaurants (rid),
+    address      varchar(50) NOT NULL,
+    open_time    TIME NOT NULL,
+    close_time   TIME NOT NULL,
+    ave_Rating   NUMERIC(2,1) NOT NULL DEFAULT 5.0,
+    contacts     NUMERIC(10,0) NOT NULL,
     CHECK (aveRating >= 0.0 and aveRating <= 5.0),
     primary key (rid, bid),
     foreign key (name) references restaurants (name) on delete cascade on update cascade
 );
 
-CREATE TABLE branch_managers (
-	uid     uuid primary key references users on delete cascade
-);
+
 
 CREATE TABLE customers (
-	uid     uuid primary key references users on delete cascade
+	uid         uuid primary key references users on delete cascade
 );
 
 CREATE TABLE assigns (
@@ -70,24 +65,24 @@ CREATE TABLE manages (
 );
 
 CREATE TABLE opens (
-    rid   uuid NOT NULL,
-    bid   uuid NOT NULL,
+    rid     uuid NOT NULL,
+    bid     uuid NOT NULL,
     primary key (rid, bid),
     foreign key (rid) references restaurants (rid) on delete cascade,
     foreign key (bid) references branches (bid)
 );
 
 CREATE TABLE registers (
-    uid   uuid NOT NULL,
-    rid   uuid NOT NULL,
+    uid     uuid NOT NULL,
+    rid     uuid NOT NULL,
     primary key (uid, rid),
     foreign key (uid) references restaurant_managers (uid),
     foreign key (rid) references restaurants (rid)
 );
 
 CREATE TABLE categories (
-    cid    uuid DEFAULT uuid_generate_v4 (),
-    name   varchar(50) NOT NULL,
+    cid     uuid DEFAULT uuid_generate_v4 (),
+    name    varchar(50) NOT NULL,
     primary key (cid)
 );
 
@@ -100,9 +95,9 @@ CREATE TABLE belongs (
 );
 
 CREATE TABLE menus (
-    name  varchar(50),
-    mid   uuid UNIQUE DEFAULT uuid_generate_v4 (),
-    bid   uuid NOT NULL,
+    mid     uuid UNIQUE DEFAULT uuid_generate_v4 (),
+    bid     uuid NOT NULL,
+    name    varchar(50),
     primary key (mid, bid),
     foreign key (bid) references branches (bid) on delete cascade
 );
@@ -116,27 +111,27 @@ CREATE TABLE provides (
 );
 
 CREATE TABLE items (
-    iid     uuid UNIQUE NOT NULL DEFAULT uuid_generate_v4 (),
-    name    varchar(50) NOT NULL,
-    price   NUMERIC(3,2) NOT NULL,
+    iid         uuid UNIQUE NOT NULL DEFAULT uuid_generate_v4 (),
+    name        varchar(50) NOT NULL,
+    price       NUMERIC(3,2) NOT NULL,
     description text,
-    mid     uuid NOT NULL,
+    mid         uuid NOT NULL,
     primary key (iid, mid),
     foreign key (mid) references menus (mid) on delete cascade
 );
 
 CREATE TABLE contains (
-    iid     uuid NOT NULL,
-    mid     uuid NOT NULL,
+    iid         uuid NOT NULL,
+    mid         uuid NOT NULL,
     primary key (iid, mid),
     foreign key (iid) references items (iid),
     foreign key (mid) references menus (mid) on delete cascade
 );
 
 CREATE TABLE reservations (
-    resid   uuid DEFAULT uuid_generate_v4 (),
-    restime timestamp NOT NULL,
-    numpeople integer NOT NULL,
+    resid       uuid DEFAULT uuid_generate_v4 (),
+    restime     timestamp NOT NULL,
+    numpeople   integer NOT NULL,
     --bid     char(36) NOT NULL,
     primary key (resid)
     --, foreign key (bid) references branches.bid
@@ -173,11 +168,8 @@ CREATE TABLE earns (
 );
 
 INSERT INTO users (username, password_hash, first_name, last_name)
-VALUES ('meeple', '$2b$10$13BWk/6YJ4JYlxPvkNTnqeT6J8zsPTe592QIen.Le7apc921uebUW', 'Adi', 'Prabawa');
+VALUES ('spring', '$2b$10$13BWk/6YJ4JYlxPvkNTnqeT6J8zsPTe592QIen.Le7apc921uebUW', 'Sprint', 'Season');
 INSERT INTO users (username, password_hash, first_name, last_name)
-VALUES ('adi'   , '$2b$10$Pdcb3BDaN1wATBHyZ0Fymurw1Js01F9nv6xgff42NfOmTrdXT1A.i', 'Mikal', 'Lim');
+VALUES ('summer', '$2b$10$Pdcb3BDaN1wATBHyZ0Fymurw1Js01F9nv6xgff42NfOmTrdXT1A.i', 'Summer', 'Season');
 INSERT INTO users (username, password_hash, first_name, last_name)
-VALUES ('cs2102', '$2b$10$vS4KkX8uenTCNooir9vyUuAuX5gUhSGVql8yQdsDDD4TG8bSUjkt.', 'Kueider', 'Ho');
-
--- INSERT INTO contacts (first_name, last_name, email, phone)
--- VALUES ('John', 'Smith', 'john.smith@example.com', '408-237-2345');
+VALUES ('autumn', '$2b$10$vS4KkX8uenTCNooir9vyUuAuX5gUhSGVql8yQdsDDD4TG8bSUjkt.', 'Autumn', 'Season');

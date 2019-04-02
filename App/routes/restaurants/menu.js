23f151f56fc9f88c7e9638b87da7ecd51d127058
jasonvank <fwenkai.jason@gmail.com>
2019-04-02 17:04:40 +0800

    fix frontend header bug

--- App/routes/restaurants/menu.js
+++ App/routes/restaurants/menu.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET menu page. */
 router.get('/', function(req, res, next) {
-  res.render('restaurants/menu', { title: 'Express' });
+  res.render('restaurants/menu', {user: req.user});
 });
 
 module.exports = router;

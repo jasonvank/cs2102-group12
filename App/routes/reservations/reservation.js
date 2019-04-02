8ef7d7ce25e69b0d1943a3bf6b14d5865be737d2
hotarumin <hotarumin98@gmail.com>
2019-04-02 15:00:01 +0800

    redirect if not logged in

--- App/routes/reservations/reservation.js
+++ App/routes/reservations/reservation.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+var flash = require('connect-flash');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+
+const { Pool } = require('pg')
+const pool = new Pool({connectionString: process.env.DATABASE_URL});
 
 /* GET reservation page. */
 router.get('/', function(req, res, next) {
-  res.render('reservations/reservation', { title: 'Express' });
+	if (!req.isAuthenticated()){
+        res.redirect('/login');
+        console.log("not logged in");
+	}
+    res.render('reservations/reservation', { title: 'Express' });
+
 });
+
+router.get('/', function(req, res, next) {
+  res.render('reservation', {user: req.user});
+});
+
 
 module.exports = router;

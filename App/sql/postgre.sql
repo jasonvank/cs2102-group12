46088b7443edffbfce51b074a843db84b3a00c00
leo-1997 <36431769+leo-1997@users.noreply.github.com>
2019-04-05 16:37:13 +0800

    change SQL table

--- App/sql/postgre.sql
+++ App/sql/postgre.sql
@@ -10,33 +10,27 @@ CREATE TABLE users (
     unique(username)
 );
 
-CREATE TABLE branch_managers (
-	uid     uuid primary key references users on delete cascade
-);
-
 CREATE TABLE restaurant_managers (
 	uid     uuid primary key references users on delete cascade
 );
 
-CREATE TABLE restaurants (
-    rid     uuid DEFAULT uuid_generate_v4 (),
-    uid     uuid NOT NULL,
-    name    varchar(50) UNIQUE NOT NULL,
-    primary key (rid),
-    foreign key (uid) references restaurant_managers (uid)
-);
+-- CREATE TABLE restaurants (
+--     rid     uuid DEFAULT uuid_generate_v4 (),
+--     uid     uuid NOT NULL,
+--     name    varchar(50) UNIQUE NOT NULL,
+--     primary key (rid),
+--     foreign key (uid) references restaurant_managers (uid)
+-- );
 
-CREATE TABLE branches (
-    bid          uuid UNIQUE DEFAULT uuid_generate_v4 (),
+CREATE TABLE restaurants (
+    rid          uuid UNIQUE DEFAULT uuid_generate_v4 (),
     name         varchar(50) UNIQUE NOT NULL,
-    uid          uuid NOT NULL,
+    uid          uuid UNIQUE NOT NULL,
     address      varchar(50) NOT NULL,
     open_time    TIME NOT NULL,
     close_time   TIME NOT NULL,
-    ave_Rating   NUMERIC(2,1) NOT NULL DEFAULT 5.0,
     contacts     NUMERIC(10,0) NOT NULL,
-    CHECK (ave_Rating >= 0.0 and ave_Rating <= 5.0),
-    primary key (bid),
+    primary key (rid),
     foreign key (uid) references restaurant_managers (uid)
     -- foreign key (name) references restaurants (name) on delete cascade on update cascade
 );
@@ -47,31 +41,31 @@ CREATE TABLE customers (
 	uid         uuid primary key references users on delete cascade
 );
 
-CREATE TABLE assigns (
-    brid   uuid,
-    rmid   uuid,
-    bid    uuid,
-    primary key (brid, rmid, bid),
-    foreign key (bid) references branches (bid),
-    foreign key (brid) references branch_managers (uid),
-    foreign key (rmid) references restaurant_managers (uid)
-);
+-- CREATE TABLE assigns (
+--     brid   uuid,
+--     rmid   uuid,
+--     bid    uuid,
+--     primary key (brid, rmid, bid),
+--     foreign key (bid) references branches (bid),
+--     foreign key (brid) references branch_managers (uid),
+--     foreign key (rmid) references restaurant_managers (uid)
+-- );
 
-CREATE TABLE manages (
-    uid     uuid NOT NULL,
-    bid     uuid NOT NULL,
-    primary key (uid, bid),
-    foreign key (uid) references branch_managers (uid),
-    foreign key (bid) references branches (bid)
-);
+-- CREATE TABLE manages (
+--     uid     uuid NOT NULL,
+--     bid     uuid NOT NULL,
+--     primary key (uid, bid),
+--     foreign key (uid) references branch_managers (uid),
+--     foreign key (bid) references branches (bid)
+-- );
 
-CREATE TABLE opens (
-    rid     uuid NOT NULL,
-    bid     uuid NOT NULL,
-    primary key (rid, bid),
-    foreign key (rid) references restaurants (rid) on delete cascade,
-    foreign key (bid) references branches (bid)
-);
+-- CREATE TABLE opens (
+--     rid     uuid NOT NULL,
+--     bid     uuid NOT NULL,
+--     primary key (rid, bid),
+--     foreign key (rid) references restaurants (rid) on delete cascade,
+--     foreign key (bid) references branches (bid)
+-- );
 
 CREATE TABLE registers (
     uid     uuid NOT NULL,
@@ -97,19 +91,19 @@ CREATE TABLE belongs (
 
 CREATE TABLE menus (
     mid     uuid UNIQUE DEFAULT uuid_generate_v4 (),
-    bid     uuid NOT NULL,
+    rid     uuid NOT NULL,
     name    varchar(50),
-    primary key (mid, bid),
-    foreign key (bid) references branches (bid) on delete cascade
+    primary key (mid, rid, name),
+    foreign key (rid) references restaurants (rid) on delete cascade
 );
 
-CREATE TABLE provides (
-    bid     uuid NOT NULL,
-    mid     uuid NOT NULL,
-    primary key (bid, mid),
-    foreign key (bid) references branches (bid) on delete cascade,
-    foreign key (mid) references menus (mid)
-);
+-- CREATE TABLE provides (
+--     rid     uuid NOT NULL,
+--     mid     uuid NOT NULL,
+--     primary key (rid, mid),
+--     foreign key (rid) references restaurants (rid) on delete cascade,
+--     foreign key (mid) references menus (mid)
+-- );
 
 CREATE TABLE items (
     iid         uuid UNIQUE NOT NULL DEFAULT uuid_generate_v4 (),
@@ -117,33 +111,33 @@ CREATE TABLE items (
     price       NUMERIC(3,2) NOT NULL,
     description text,
     mid         uuid NOT NULL,
-    primary key (iid, mid),
+    primary key (iid, mid, name),
     foreign key (mid) references menus (mid) on delete cascade
 );
 
-CREATE TABLE contains (
-    iid         uuid NOT NULL,
-    mid         uuid NOT NULL,
-    primary key (iid, mid),
-    foreign key (iid) references items (iid),
-    foreign key (mid) references menus (mid) on delete cascade
-);
+-- CREATE TABLE contains (
+--     iid         uuid NOT NULL,
+--     mid         uuid NOT NULL,
+--     primary key (iid, mid),
+--     foreign key (iid) references items (iid),
+--     foreign key (mid) references menus (mid) on delete cascade
+-- );
 
 CREATE TABLE reservations (
     resid       uuid DEFAULT uuid_generate_v4 (),
     restime     timestamp NOT NULL,
     numpeople   integer NOT NULL,
-    --bid     char(36) NOT NULL,
+    --rid     char(36) NOT NULL,
     primary key (resid)
-    --, foreign key (bid) references branches.bid
+    --, foreign key (rid) references restaurants.rid
 );
 
 CREATE TABLE processes (
     resid     uuid NOT NULL,
-    bid       uuid NOT NULL,
-    primary key (resid, bid),
+    rid       uuid NOT NULL,
+    primary key (resid, rid),
     foreign key (resid) references reservations (resid),
-    foreign key (bid) references branches (bid)
+    foreign key (rid) references restaurants (rid)
 );
 
 CREATE TABLE books (
@@ -166,6 +160,21 @@ CREATE TABLE earns (
     primary key (rewid, uid),
     foreign key (rewid) references rewards (rewid),
     foreign key (uid) references customers (uid)
+);
+
+CREATE TABLE ratings (
+    rid     uuid NOT NULL,
+    ave_Rating   NUMERIC(2,1) NOT NULL DEFAULT 5.0,
+    CHECK (ave_Rating >= 0.0 and ave_Rating <= 5.0),
+    primary key (rid),
+    foreign key (rid) references restaurants (rid) on delete cascade
+);
+
+CREATE TABLE rate (
+  resid uuid NOT NULL references reservations(resid),
+  uid uuid NOT NULL references customers(uid) on delete cascade,
+  rid uuid NOT NULL references restaurants(rid),
+  primary key (resid, uid, rid)
 );
 
 INSERT INTO users (username, password_hash, first_name, last_name)

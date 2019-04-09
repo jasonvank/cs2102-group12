985f6542114d0f122be2745d6976138c782c93de
leo-1997 <36431769+leo-1997@users.noreply.github.com>
2019-04-09 20:29:09 +0800

    fix selection bug

--- App/routes/restaurants/restaurant.js
+++ App/routes/restaurants/restaurant.js
@@ -28,6 +28,7 @@ router.post('/', selectRestaurant);
 
 function selectRestaurant(req, res, next) {
   var restaurant_name = req.body.restaurant_name;
+  console.log(restaurant_name);
   pool.query(sql_query.query.restaurant_rid, [restaurant_name], (err, data) => {
     if(err) return(next);
     restaurantRid = data.rows[0].rid;
